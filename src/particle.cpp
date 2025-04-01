43abf58ee98b3f20c06cdb84b9153d80e7ecacf0
Bruno Celiz <bruno.celiz2303@gmail.com>
2025-04-01 16:10:49 -0300

    Reduced size of struct Particle. Now pressure and dens^-1 are computed on-the-fly, not saved.

--- src/particle.cpp
+++ src/particle.cpp
@@ -5,13 +5,13 @@
 de las densidades, más vale las calculemos on-the-fly; ¿Ídem Energía/AngMom? */
 Particle::Particle()
  : mMass(1.0f),
-   mDensity(0.0f),
-   mDensityInverse(0.0f),
-   mDensityInverse2(0.0f),
-   mPressure(0.0f),
-   mPotentialEnergy(0.0f),
-   mKineticEnergy(0.0f),
-   mAngularMomentum(0.0f, 0.0f, 0.0f)
+   mDensity(0.0f)//,
+   //mDensityInverse(0.0f),
+   //mDensityInverse2(0.0f),
+   //mPressure(0.0f),
+   //mPotentialEnergy(0.0f),
+   //mKineticEnergy(0.0f),
+   //mAngularMomentum(0.0f, 0.0f, 0.0f)
 {
    mVelocity.set(0.0f, 0.0f, 0.0f);
    mAcceleration.set(0.0f, 0.0f, 0.0f);
